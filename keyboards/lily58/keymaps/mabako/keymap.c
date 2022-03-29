58df803e37fb112e5b2a970910513227434bcad7
Marcus Bauer <marcus@bauer.wf>
2022-03-29 17:49:59 +0200

    key overrides

--- keyboards/lily58/keymaps/mabako/keymap.c
+++ keyboards/lily58/keymaps/mabako/keymap.c
@@ -13,11 +13,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Z  |   U  |   I  |   O  |   P  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * |LCTRL |   Y  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -26,9 +26,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Z,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+  KC_LCTRL, KC_Y,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                         KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RALT
 ),
 /* LOWER
@@ -147,3 +147,29 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
+
+const key_override_t undo_override = ko_make_basic(MOD_MASK_CTRL, KC_Y, C(KC_Z));
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+// umlaute
+const key_override_t ae_override = ko_make_basic(MOD_BIT(KC_RALT), KC_A, RALT(KC_Q));
+const key_override_t oe_override = ko_make_basic(MOD_BIT(KC_RALT), KC_O, RALT(KC_P));
+const key_override_t ue_override = ko_make_basic(MOD_BIT(KC_RALT), KC_U, RALT(KC_Y));
+const key_override_t eur_override = ko_make_basic(MOD_BIT(KC_RALT), KC_E, RALT(KC_5));
+// brackets
+const key_override_t lparen_override = ko_make_with_layers_and_negmods(0, KC_LBRC, KC_LEFT_PAREN, ~0, MOD_MASK_CSAG);
+const key_override_t rparen_override = ko_make_with_layers_and_negmods(0, KC_RBRC, KC_RIGHT_PAREN, ~0, MOD_MASK_CSAG);
+const key_override_t lbrc_override = ko_make_with_layers_and_negmods(MOD_MASK_CTRL, KC_LBRC, KC_LBRC, ~0, MOD_MASK_SA);
+const key_override_t rbrc_override = ko_make_with_layers_and_negmods(MOD_MASK_CTRL, KC_RBRC, KC_RBRC, ~0, MOD_MASK_SA);
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &undo_override,
+    &delete_key_override,
+    &ae_override,
+    &oe_override,
+    &ue_override,
+    &eur_override,
+    &lparen_override,
+    &rparen_override,
+    &lbrc_override,
+    &rbrc_override,
+    NULL
+};
