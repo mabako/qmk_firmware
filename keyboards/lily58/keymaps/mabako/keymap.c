9877f8b076e9beabce2c1d3da28b37c4d9d22bd7
Marcus Bauer <marcus@bauer.wf>
2022-04-12 02:41:10 +0200

    raw hid test command

--- keyboards/lily58/keymaps/mabako/keymap.c
+++ keyboards/lily58/keymaps/mabako/keymap.c
@@ -18,6 +18,8 @@ enum custom_keycodes {
   C_SHRUG = SAFE_RANGE,
   C_TFLIP,
   C_TPUT,
+
+  CH_TEST
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -81,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT(
-  _______,       _______, _______, _______, _______, _______,                     QK_BOOT, _______, KC_MPLY, KC_MNXT, KC_PSCR, KC_PAUS,
+  _______,       _______, _______, _______, _______, CH_TEST,                     QK_BOOT, _______, KC_MPLY, KC_MNXT, KC_PSCR, KC_PAUS,
   KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
   LSFT_T(KC_F1), KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_RSFT,
   LCTL_T(KC_F7), KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, RCTL_T(KC_BSLS),
@@ -156,6 +158,7 @@ const char *read_keylogs(void);
 // const char *read_host_led_state(void);
 // void set_timelog(void);
 // const char *read_timelog(void);
+void send_hid_command(uint8_t command);
 
 bool oled_task_user(void) {
   if (!is_keyboard_left()) {
@@ -274,6 +277,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         send_unicode_string("┬──┬ \u30CE( ゜-゜\u30CE)");
       }
       return false;
+    case CH_TEST:
+      if (record->event.pressed) {
+        send_hid_command(0xF1);
+      }
+      return false;
   }
 
   return true;
@@ -295,11 +303,15 @@ void clock_set_handler(uint8_t request_size, const void* request, uint8_t respon
 }
 
 void raw_hid_receive(uint8_t *data, uint8_t length) {
+  if (data[0] != 0xC3) {
+    return;
+  }
+
   uint8_t response[RAW_EPSIZE];
   memset(response, 0, RAW_EPSIZE);
 
-  if(data[0] == 0x01) {
-    uint32_t ms_since_midnight = (((uint32_t)data[1] << 24) | ((uint32_t)data[2] << 16) | ((uint32_t)data[3] << 8) | (uint32_t)data[4]);
+  if(data[1] == 0x01) {
+    uint32_t ms_since_midnight = (((uint32_t)data[2] << 24) | ((uint32_t)data[3] << 16) | ((uint32_t)data[4] << 8) | (uint32_t)data[5]);
     if (is_keyboard_left()) {
       clock_set_handler(sizeof(ms_since_midnight), (void*)&ms_since_midnight, 0, NULL);
     } else {
@@ -307,9 +319,19 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
       clock_set = true;
     }
 
-    response[0] = 0x01;
+    response[0] = 0xC4;
+    response[1] = 0x01;
   }
   
+  raw_hid_send(response, RAW_EPSIZE);
+}
+
+void send_hid_command(uint8_t command) {
+  uint8_t response[RAW_EPSIZE];
+  memset(response, 0, RAW_EPSIZE);
+  response[0] = 0xC5;
+  response[1] = command;
+
   raw_hid_send(response, RAW_EPSIZE);
 }
 
